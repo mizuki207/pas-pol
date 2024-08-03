6db177ab59b03a8ce2d596d258f1dabbf68585e9
mizuki207 <m.k.n.b.m.k.n.b5@gmail.com>
2024-08-03 11:11:20 +0900

    <head>内の記述内容を確認。変更済

--- front-page.php
+++ front-page.php
@@ -2,34 +2,34 @@
 <html lang="ja">
 
 <head>
-   <meta charset="UTF-8">
-   <title>PAS-POL -旅のモノづくりブランド-｜TABIPPO</title>
-   <meta name="description" content="PAS-POLはTABIPPOのモノづくりブランドです。 ⾃分と世界を繋げる新しい時代のパスポートのようなモノを作りたいと思い、PAS-POLという名前をつけました。">
-   <meta name="robots" content="index,follow">
-   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta charset="UTF-8">
+  <title>PAS-POL -旅のモノづくりブランド-｜TABIPPO</title>
+  <meta name="description" content="PAS-POLはTABIPPOのモノづくりブランドです。 ⾃分と世界を繋げる新しい時代のパスポートのようなモノを作りたいと思い、PAS-POLという名前をつけました。">
+  <meta name="robots" content="index,follow">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-   <!-- OGP -->
-   <meta property="fb:app_id" content="450419765129931">
-   <meta property="og:url" content="https://pas-pol.jp">
-   <meta property="og:type" content="website">
-   <meta property="og:title" content="PAS-POL -旅のモノづくりブランド-｜TABIPPO">
-   <meta property="og:locale" content="ja_JP">
-   <meta property="og:image" content="https://pas-pol.jp/wp-content/themes/pas-pol/dist/img/ogp.jpg">
-   <meta property="og:description" content="PAS-POLはTABIPPOのモノづくりブランドです。 ⾃分と世界を繋げる新しい時代のパスポートのようなモノを作りたいと思い、PAS-POLという名前をつけました。">
-   <meta property="og:site_name" content="PAS-POL -旅のモノづくりブランド-｜TABIPPO">
-   <meta name="twitter:card" value="summary_large_image">
-   <link rel="dns-prefetch" href="//s.w.org">
-   <script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=G-Y41FM4JJD3&amp;cx=c&amp;_slc=1"></script><script src="https://connect.facebook.net/ja_JP/all.js?hash=e4fc7078c1f73b4c366b87bebaac5c4b" async="" crossorigin="anonymous"></script><script async="" src="//www.google-analytics.com/analytics.js"></script><script id="twitter-wjs" src="https://platform.twitter.com/widgets.js"></script><script id="facebook-jssdk" src="//connect.facebook.net/ja_JP/all.js#xfbml=1"></script><script type="text/javascript">
-     window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/pas-pol.jp\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.9.6"}};
-     !function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56692,8205,9792,65039],[55357,56692,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
-   </script>
-   <script src="https://pas-pol.jp/wp-includes/js/wp-emoji-release.min.js?ver=4.9.6" type="text/javascript" defer=""></script>
-   <link rel="https://api.w.org/" href="https://pas-pol.jp/wp-json/">
-   <link rel="stylesheet" href="https://pas-pol.jp/wp-content/themes/pas-pol/dist/css/style.css">
-   <link rel="shortcut icon" href="https://pas-pol.jp/wp-content/themes/pas-pol/dist/img/favicon.ico">
+  <!-- OGP -->
+  <meta property="fb:app_id" content="450419765129931">
+  <meta property="og:url" content="https://pas-pol.jp">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="PAS-POL -旅のモノづくりブランド-｜TABIPPO">
+  <meta property="og:locale" content="ja_JP">
+  <meta property="og:image" content="https://pas-pol.jp/wp-content/themes/pas-pol/dist/img/ogp.jpg">
+  <meta property="og:description" content="PAS-POLはTABIPPOのモノづくりブランドです。 ⾃分と世界を繋げる新しい時代のパスポートのようなモノを作りたいと思い、PAS-POLという名前をつけました。">
+  <meta property="og:site_name" content="PAS-POL -旅のモノづくりブランド-｜TABIPPO">
+  <meta name="twitter:card" value="summary_large_image">
+  <link rel="dns-prefetch" href="//s.w.org">
+  <script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=G-Y41FM4JJD3&amp;cx=c&amp;_slc=1"></script><script src="https://connect.facebook.net/ja_JP/all.js?hash=e4fc7078c1f73b4c366b87bebaac5c4b" async="" crossorigin="anonymous"></script><script async="" src="//www.google-analytics.com/analytics.js"></script><script id="twitter-wjs" src="https://platform.twitter.com/widgets.js"></script><script id="facebook-jssdk" src="//connect.facebook.net/ja_JP/all.js#xfbml=1"></script><script type="text/javascript">
+    window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/pas-pol.jp\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.9.6"}};
+    !function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56692,8205,9792,65039],[55357,56692,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
+  </script>
+  <script src="https://pas-pol.jp/wp-includes/js/wp-emoji-release.min.js?ver=4.9.6" type="text/javascript" defer=""></script>
+  <link rel="https://api.w.org/" href="https://pas-pol.jp/wp-json/">
+  <link rel="stylesheet" href="https://pas-pol.jp/wp-content/themes/pas-pol/dist/css/style.css">
+  <link rel="shortcut icon" href="https://pas-pol.jp/wp-content/themes/pas-pol/dist/img/favicon.ico">
 
-   <script src="https://pas-pol.jp/wp-content/themes/pas-pol/dist/js/lib.js"></script>
-   <script src="https://pas-pol.jp/wp-content/themes/pas-pol/dist/js/script.js"></script>
+  <script src="https://pas-pol.jp/wp-content/themes/pas-pol/dist/js/lib.js"></script>
+  <script src="https://pas-pol.jp/wp-content/themes/pas-pol/dist/js/script.js"></script>
 </head>
 
 <body class="p-index" cz-shortcut-listen="true">
@@ -145,7 +145,7 @@
       <article class="m-productList-item l-grid-4">
   <a class="m-productList-item-link" href="https://pas-pol.jp/product/365-kyoto/">
     <div class="m-productList-item-image">
-       <img src="https://s3-ap-northeast-1.amazonaws.com/paspol-images/wp-content/uploads/2020/12/kyoto365_thumnail-1.png" width="484" height="640" alt="">
+      <img src="https://s3-ap-northeast-1.amazonaws.com/paspol-images/wp-content/uploads/2020/12/kyoto365_thumnail-1.png" width="484" height="640" alt="">
     </div>
     <h2 class="m-productList-item-title">365日 京都 絶景の旅</h2>
     <p class="m-productList-item-description"></p>
@@ -156,7 +156,7 @@
       <article class="m-productList-item l-grid-4">
   <a class="m-productList-item-link" href="https://pas-pol.jp/product/365-hokkaido-calendar/">
     <div class="m-productList-item-image">
-       <img src="https://s3-ap-northeast-1.amazonaws.com/paspol-images/wp-content/uploads/2020/09/banner_thumb1.png" width="484" height="640" alt="">
+      <img src="https://s3-ap-northeast-1.amazonaws.com/paspol-images/wp-content/uploads/2020/09/banner_thumb1.png" width="484" height="640" alt="">
     </div>
     <h2 class="m-productList-item-title">365日 北海道 絶景日めくりカレンダー</h2>
     <p class="m-productList-item-description"></p>
