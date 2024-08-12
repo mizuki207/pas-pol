4a37be067963a02566ec1e4ffc18da00775184c0
mizuki207 <m.k.n.b.m.k.n.b5@gmail.com>
2024-08-12 20:14:49 +0900

    newsのアーカイブページ内を作成
    プラグイン｢Advanced Custom Fields｣で投稿ページを作成
    
    scssフォルダを作成して管理しやすくまとめました
    
    いまいち投稿ページがうまく作成できているのか分からない状況です。また、single.phpファイルはアーカイブページ内の投稿内容から遷移した先のページ、、、つまりarchive-newsよりも階層が一つ下のページになるのでしょうか？

---
+++ archive-product.php
@@ -0,0 +1,69 @@
+<section class="l-section l-section--product">
+  <div class="l-container l-container--narrow">
+      <div class="m-section__title l-row">
+        <h1 class="m-section__title--text">PRODUCT</h1>
+      </div>
+
+    <div class="m-product__list l-row">
+      <article class="m-product__list-item l-grid-4">
+        <a class="m-product__list-item-link" href="">
+          <div class="m-product__list-item-image">
+            <img  src="<?php echo esc_url (get_template_directory_uri() . '/assets/images/wh_thumb2.webp' ); ?>" 
+            width="484" height="640" alt="プロダクトの画像1">
+          </div>
+          <h2 class="m-product__list-item-title">地球一周 365日 世界遺産絶景の旅</h2>
+          <p class="m-product__list-item-description"></p>
+        </a>
+      </article>
+      <article class="m-product__list-item l-grid-4">
+        <a class="m-product__list-item-link" href="">
+          <div class="m-product__list-item-image">
+            <img src="<?php echo esc_url (get_template_directory_uri() . '/assets/images/kyoto365_thumnail-1.webp' ); ?>" 
+            width="484" height="640" alt="プロダクトの画像2">
+          </div>
+          <h2 class="m-product__list-item-title">365日 京都 絶景の旅</h2>
+          <p class="m-product__list-item-description"></p>
+        </a>
+      </article>
+      <article class="m-product__list-item l-grid-4">
+        <a class="m-product__list-item-link" href="">
+          <div class="m-product__list-item-image">
+            <img src="<?php echo esc_url (get_template_directory_uri() . '/assets/images/banner_thumb1.webp' ); ?>" 
+            width="484" height="640" alt="プロダクトの画像1">
+          </div>
+          <h2 class="m-product__list-item-title">365日 北海道 絶景日めくりカレンダー</h2>
+          <p class="m-product__list-item-description"></p>
+        </a>
+      </article>
+      <article class="m-product__list-item l-grid-4">
+        <a class="m-product__list-item-link" href="">
+          <div class="m-product__list-item-image">
+            <img src="<?php echo esc_url (get_template_directory_uri() . '/assets/images/thumb_hokkaido.webp' ); ?>" 
+            width="484" height="640" alt="プロダクトの画像1">
+          </div>
+          <h2 class="m-product__list-item-title">365日 北海道 絶景の旅</h2>
+          <p class="m-product__list-item-description"></p>
+      </article>
+      <article class="m-product__list-item l-grid-4">
+        <a class="m-product__list-item-link" href="">
+          <div class="m-product__list-item-image">
+            <img src="<?php echo esc_url (get_template_directory_uri() . '/assets/images/thumb_fes.webp' ); ?>"
+              width="484" height="640" alt="プロダクトの画像1">
+          </div>
+          <h2 class="m-product__list-item-title">The World Festival Guide -海外の音楽フェス完全ガイド-</h2>
+          <p class="m-product__list-item-description"></p>
+        </a>
+      </article>
+      <article class="m-product__list-item l-grid-4">
+        <a class="m-product__list-item-link" href="">
+          <div class="m-product__list-item-image">
+            <img src="<?php echo esc_url (get_template_directory_uri() . '/assets/images/banner_サムネイル21.webp' ); ?>" 
+            width="484" height="640" alt="プロダクトの画像1">
+          </div>
+          <h2 class="m-product__list-item-title">この世界で死ぬまでにしたいこと2000</h2>
+          <p class="m-product__list-item-description"></p>
+        </a>
+      </article>
+    </div>
+  </div>
+</section>
---
+++ single-news.php
@@ -0,0 +1,3 @@
+<?php
+echo get_permalink(33);
+?>
