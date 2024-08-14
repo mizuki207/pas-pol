3d17c30a94bb3c5da8eed1265c46ba3d9fa6feef
mizuki207 <m.k.n.b.m.k.n.b5@gmail.com>
2024-08-14 20:04:58 +0900

    プラグインCustom Field Suiteを使用してproductの投稿ページとフィールドカスタム設定をしました

--- single-news.php
+++ single-news.php
@@ -4,25 +4,24 @@
 $date = get_field('date');
 ?>
 <dl>
-  <dd><?php echo date('Y.m.d', strtotime($date)); ?>
- </dd>
+  <dd><?php echo date('Y.m.d', strtotime($date)); ?></dd>
 </dl>
 
-<img src="<?php the_field('news_img_field'); ?>" >
+<img src="<?php the_field('news_img-1'); ?>" >
 
 <p><?php the_field('text_filed'); ?></p>
 
-<img src="<?php the_field('news_img_field'); ?>" >
+<img src="<?php the_field('news_img-2'); ?>" >
 
 <p><?php the_field('text_filed'); ?></p>
 
-<img src="<?php the_field('news_img_field'); ?>" >
+<img src="<?php the_field('news_img-3'); ?>" >
 
 <h3><?php the_field('title'); ?></h3>
 
 <p><?php the_field('recruitment_plans'); ?></p>
 
-<img src="<?php the_field('news_img_field'); ?>" >
+<img src="<?php the_field('news_img-4'); ?>" >
 
 <h3><?php the_field('title_text_filed'); ?></h3>
 
---
+++ single-product.php
@@ -0,0 +1,6 @@
+<?php echo CFS()->get('title'); ?>
+<?php echo CFS()->get('text'); ?>
+<img src="<?php echo CFS()->get('img'); ?>" >
+<?php echo CFS()->get('sub_title'); ?>
+<?php echo CFS()->get('sub_title-text'); ?>
+<img src="<?php echo CFS()->get('sub_img'); ?>" >
